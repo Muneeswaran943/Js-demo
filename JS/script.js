746b72c9c7f9e7cc224997ec1906e80b361a30a1
Muneeswaran943 <149227632+Muneeswaran943@users.noreply.github.com>
2023-10-28 12:29:35 +0530

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+//alert("welcome to Js for External File")
+
+console.log("welcome to Tutor Joes");
+console.log(123456);
+console.log(18.5);
+console.log(true);
+console.log([58,78,96,35]);
+console.log({fname:'Joes',age:25});
+console.table({fname:'Joes',age:25});
+console.error("Custom Sample Error");
+console.warn("Custom Sample Error");
+
